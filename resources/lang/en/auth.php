f0b3614dd1395c7bfc618efdcb568faaf2c182ef
ebilim <52946415+ebilim@users.noreply.github.com>
2021-08-15 16:50:32 +0500

    resources/lang/en/auth edited

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,29 @@ return [
     |
     */
 
+    'app' => [
+        'member_since' => 'Member since',
+    ],
+    'confirm_password' => 'Confirm password',
+    'email' => 'Email',
     'failed' => 'These credentials do not match our records.',
+    'full_name' => 'Full name',
+    'login' => [
+        'forgot_password' => 'Forgot password',
+        'register_membership' => 'Register membership',
+        'title' => 'Login',
+    ],
     'password' => 'The provided password is incorrect.',
+    'register' => 'Register',
+    'registration' => [
+        'have_membership' => 'Have membership',
+        'i_agree' => 'I agree with ',
+        'terms' => 'Terms',
+        'title' => 'Register new user',
+    ],
+    'remember_me' => 'Remember me',
+    'sign_in' => 'Sing in',
+    'sign_out' => 'Sing out',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
