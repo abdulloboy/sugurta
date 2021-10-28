a1d13dc94db3aaf7e1fffad17466cb803e15d015
abdulloboy <89206946+abdulloboy@users.noreply.github.com>
2021-10-28 16:06:52 +0500

    migration updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            "name" => "Abdulloboy",
+            "email" => "abdulloboy5@gmail.com",
+            "password" => Hash::make("password")
+        ]);
         $this->call([
             ObyektSeeder::class,
             OqibatSeeder::class,
